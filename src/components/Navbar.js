a97a0c0926247b17fc4ecef957d76a8ad5f41c80
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2025-03-21 16:52:30 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,69 @@
-import '../style/styleNavbar.css';
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import { faHome, faAddressCard } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { styled } from '@mui/material/styles';
+import { Link } from '@mui/material';
 
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+  position: 'fixed',
+  top: '2%',
+  left: 0,
+  right: 0,
+  width: 'auto',
+  marginLeft: '4.5%',
+  marginRight: '4.5%',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  padding: '15px',
+  borderRadius: '25px',
+  zIndex: 1000,
+  backdropFilter: 'blur(5px)',
+  backgroundColor: 'rgba(255, 255, 255, 0.2)', // Aggiungi uno sfondo trasparente
+  [theme.breakpoints.down('sm')]: {
+    top: '88%',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(21, 64, 70, 0.509)',
+    marginLeft: 0,
+    marginRight: 0,
+    padding: '10px',
+  },
+}));
 
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  fontSize: '1rem',
+  margin: '0 20px',
+  borderRadius: '25px',
+  color: '#F7F7F7',
+  width: '10%',
+  '&:hover': {
+    color: '#F7F7F7',
+  },
+  [theme.breakpoints.down('sm')]: {
+    width: '20%',
+    fontSize: '1.5rem',
+    backgroundColor: 'rgba(228, 228, 228, 0)',
+    padding: '10px',
+  },
+}));
 
 function Navbar() {
-   
-    return (
-        <>
-            <div id="navbar">
-                <button className="option-menu"><a href="#first"><FontAwesomeIcon icon={faHome}/></a></button>
-                <button className="option-menu"><a href="#about"><FontAwesomeIcon icon={faAddressCard}/></a></button>
-            </div>
-        </>
-    );
-
+  return (
+    <StyledAppBar position="static">
+      <Toolbar>
+        <StyledIconButton component={Link} href="#first">
+          <FontAwesomeIcon icon={faHome} />
+        </StyledIconButton>
+        <StyledIconButton component={Link} href="#about">
+          <FontAwesomeIcon icon={faAddressCard} />
+        </StyledIconButton>
+      </Toolbar>
+    </StyledAppBar>
+  );
 }
 
 export default Navbar;
