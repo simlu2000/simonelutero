37a039118e534bee809953be332e8da98d53c126
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2025-04-01 17:21:57 +0200

    UPds

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../style/style.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'; // Importa le icone dei brand
