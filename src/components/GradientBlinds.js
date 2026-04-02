e08fcdcb2b14470d123469c575b7a3a17ff7548a
Simone Lutero <73953810+simlu2000@users.noreply.github.com>
2026-04-02 09:53:38 +0200

    Update GradientBlinds.js

--- src/components/GradientBlinds.js
+++ src/components/GradientBlinds.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { Renderer, Program, Mesh, Triangle } from 'ogl';
-import '../style/GradientBlinds.css';
+import '../style/gradientBlinds.css';
 
 const MAX_COLORS = 8;
 const hexToRGB = hex => {
